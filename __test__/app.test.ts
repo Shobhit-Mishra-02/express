8274bdb00e0915564adda7fed3e8b02142a4a456
shobhit <shobhitmishra2002@gmail.com>
2022-09-27 21:51:39 +0530

    added some changes

--- __test__/app.test.ts
+++ __test__/app.test.ts
@@ -1,6 +1,11 @@
 import app from "../app";
 import request from "supertest";
 
+/*
+Before running tests, run this command:
+$ npx ts-jest config:init
+*/
+
 describe("Test app.ts", () => {
   test("GET /", async () => {
     const res = await request(app).get("/");
